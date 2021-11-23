abe3b117f5e8dde8a301b5d21965f4be505c0e69
Edwin Edgardo Lopez <edgardolopez.170@gmail.com>
2021-11-23 16:04:36 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var http = require('http');
 var fs = require('fs');
 
+var express = require('express'); 
+var app = express();
+
+app.use(express.static(__dirname + '/client'));
+
 const port = process.env.PORT || 3000
 
 http.createServer(function(req, res){
