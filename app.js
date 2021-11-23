bd114d5423629a2f8287ae4ad546417c692b093f
Edwin Edgardo Lopez <edgardolopez.170@gmail.com>
2021-11-23 14:38:36 -0600

    fix

--- app.js
+++ app.js
@@ -4,11 +4,35 @@ var fs = require('fs');
 const port = process.env.PORT || 3000
 
 http.createServer(function(req, res){
-  fs.readFile('./client/index.html',function (err, data){
+  if(req.url.indexOf('.html') != -1){
+    fs.readFile('./client/index.html',function (err, data){
+        res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
+        res.write(data);
+        res.end();
+    });
+  }
+
+  if(req.url.indexOf('.css') != -1){
+    fs.readFile('./client/index.html',function (err, data){
+        res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
+        res.write(data);
+        res.end();
+    });
+  }
+
+  if(req.url.indexOf('.js') != -1){
+    fs.readFile('./client/app.html',function (err, data){
+        res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
+        res.write(data);
+        res.end();
+    });
+
+    fs.readFile('./client/ui.js.html',function (err, data){
       res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
       res.write(data);
       res.end();
   });
+  }
 }).listen(port);
 
 
