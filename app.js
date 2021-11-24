db2bdf65111909483b9b102bd991b7d8e7c3a833
Edwin Edgardo Lopez <edgardolopez.170@gmail.com>
2021-11-23 18:07:05 -0600

    Update app.js

--- app.js
+++ app.js
@@ -9,5 +9,10 @@ http.createServer(function(req, res){
         res.write(data);
         res.end();
     });
+    fs.readFile('./client/app.js',function (err, data){
+      res.writeHead(200, {'Content-Type': 'text/text/javascript','Content-Length':data.length});
+      res.write(data);
+      res.end();
+  });
 }).listen(port);
 
