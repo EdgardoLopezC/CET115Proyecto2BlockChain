08749678576e3ce363927269e9975ccb14ec1609
Edwin Edgardo Lopez <edgardolopez.170@gmail.com>
2021-11-23 19:51:26 -0600

    Update app.js

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const fs = require('fs');
 
 const port = process.env.PORT || 3000
 
-const server = http.createServer((req,res)=>{
+http.createServer((req,res)=>{
   if(req.url == '/') {
       const readStream = fs.createReadStream('./client/index.html');
       res.writeHead(200,{'Content-type': 'text/html'});
       readStream.pipe(res);
   }
 
-});
+}).listen(port);
 
